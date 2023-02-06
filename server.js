0ae46044017fa6e1757e83a15dfd0e6452f6afde
adzzy89 <adam_pilato@hotmail.com>
2023-02-06 23:45:09 +1100

    fixed variables in notes.js

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path');
-// const index = require('./routes/index.js');
-
-const PORT =  3001;
+const api = require('./routes/index.js');
 
 const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
 
 app.use(express.static('public'));
 
